24af24ebfdafb10b5dc50a9ab55adaa8ed041ee3
Harold Jackson <xela2407@gmail.com>
2018-03-04 15:07:51 -0600

    added template for files

---
+++ action.py
@@ -0,0 +1,23 @@
+# contains constructor  methods for circle actions
+class Line:
+
+	def __init__(self, start_position, end_position, poi):
+		self.start_position = start_position 
+		self.end_position = end_position 
+		self.poi = poi
+		self.batch_jobs = []
+
+
+
+
+
+# contains constructor  methods for circle actions
+class Circle: 
+	
+	def __init__(self, start_position, radius, period, poi):
+		self.start_position = start_position 
+		self.radius = radius 
+		self.poi = poi
+		self.period = period
+		self.batch_jobs = [] 
+
